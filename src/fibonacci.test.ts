1b1345637f988ae98b797f299ed65200e738a097
unknown <xsirotak@ad.fi.muni.cz>
2024-09-23 13:39:02 +0200

    Added unit test for better cover

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -1,5 +1,11 @@
 import { fibonacci } from "./fibonacci.ts";
 
-test("fibonacci-5", () => {
+test("fibonacci-5-pass", () => {
   expect(fibonacci(5)).toBe(5);
+});
+test("fibonacci-negativeNumber-fail", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
 });

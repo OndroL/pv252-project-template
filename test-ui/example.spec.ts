f61aefb8ad1a1f9cbcf272023deac76096444826
unknown <xsirotak@ad.fi.muni.cz>
2024-09-23 13:45:33 +0200

    Adding end-to-end tests for better coverage

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -5,3 +5,17 @@ test("find-watman", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
+
+test("initFibonacciUi-updates-component", async ({ page }) => {
+  await page.goto("/");
+  const fibonacciComponent = page.locator("#fibonacci-ui");
+  await expect(fibonacciComponent).toContainText("5th fibonacci number is 5");
+  await expect(fibonacciComponent.locator("code")).toContainText("5");
+});
+
+test("initFactorialUi-updates-component", async ({ page }) => {
+  await page.goto("/");
+  const factorialComponent = page.locator("#factorial-ui");
+  await expect(factorialComponent).toContainText("Factorial value 5! is 120");
+  await expect(factorialComponent.locator("code")).toContainText("120");
+});

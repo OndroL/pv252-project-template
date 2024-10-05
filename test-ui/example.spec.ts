cc9a650c87685ff23156349dcc0659ecd047281b
Ondrej Siroťák <469447@mail.muni.cz.com>
2024-10-05 22:05:58 +0200

    Fixed and added new unit/e2e tests to improve code coverage

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -6,16 +6,78 @@ test("find-watman", async ({ page }) => {
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
 
-test("initFibonacciUi-updates-component", async ({ page }) => {
+test("initial-notification", async ({ page }) => {
   await page.goto("/");
-  const fibonacciComponent = page.locator("#fibonacci-ui");
-  await expect(fibonacciComponent).toContainText("5th fibonacci number is 5");
-  await expect(fibonacciComponent.locator("code")).toContainText("5");
+  await expect(page.locator('.uk-notification-message')).toHaveText("Hello World!");
 });
 
-test("initFactorialUi-updates-component", async ({ page }) => {
+test("navigate-to-factorial", async ({ page }) => {
   await page.goto("/");
-  const factorialComponent = page.locator("#factorial-ui");
-  await expect(factorialComponent).toContainText("Factorial value 5! is 120");
-  await expect(factorialComponent.locator("code")).toContainText("120");
+
+  await page.waitForTimeout(3000);
+
+  await page.click("#site-a");
+
+  const waitForNotificationText = async (expectedText: string) => {
+    await page.waitForFunction(
+      (expectedText) => {
+        const notifications = document.querySelectorAll('.uk-notification-message');
+        for (const notification of notifications) {
+          const text = notification?.textContent?.trim();
+          console.log("Notification text:", text);
+          if (text === expectedText) return true;
+        }
+        return false;
+      },
+      expectedText,
+      { timeout: 10000 }
+    );
+  };
+
+  await waitForNotificationText("Going to factorials in 3s...");
+
+
+  await page.waitForTimeout(1000);
+  await waitForNotificationText("Going to factorials in 2s...");
+
+  await page.waitForTimeout(1000);
+  await waitForNotificationText("Going to factorials in 1s...");
+
+  await page.waitForTimeout(2000);
+  await expect(page).toHaveURL("/site_a.html");
+});
+
+test("navigate-to-fibonacci", async ({ page }) => {
+  await page.goto("/");
+
+  await page.waitForTimeout(3000);
+
+  await page.click("#site-b");
+
+  const waitForNotificationText = async (expectedText: string) => {
+    await page.waitForFunction(
+      (expectedText) => {
+        const notifications = document.querySelectorAll('.uk-notification-message');
+        for (const notification of notifications) {
+          const text = notification?.textContent?.trim();
+          console.log("Notification text:", text);
+          if (text === expectedText) return true;
+        }
+        return false;
+      },
+      expectedText,
+      { timeout: 10000 }
+    );
+  };
+  await waitForNotificationText("Going to fibonacci in 3s...");
+
+  await page.waitForTimeout(1000);
+  await waitForNotificationText("Going to fibonacci in 2s...");
+
+  await page.waitForTimeout(1000);
+  await waitForNotificationText("Going to fibonacci in 1s...");
+
+
+  await page.waitForTimeout(1000);
+  await expect(page).toHaveURL("/site_b.html");
 });
